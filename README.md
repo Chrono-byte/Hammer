4f2df53b12ed31f875b88bc3585ce452f24991d1
Chrono <me@disilla.org>
2023-01-18 21:31:45 -0500

    server update

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Hammer
+
+Hammer is a simple WebSocket-based chat server & client written in JavaScript.
+
+This mono-repo contains both the server and client portions of Hammer.
+
+`boiler` is the chat server implementing all server-side chat and UAC `user account control` commands.
+
+`traincar` is the web-based client. Currently it is raw HTML and JavaScript, no fancy libraries.
+
+## Installation
+
+Install the dependencies:
+
+```bash
+npm install
+```
+
+## Usage
+
+Run the server:
+
+```bash
+cd server/
+yarn
+node .
+```
+
+Run the client open it in your browser
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+Hammer uses different licenses for the server and client.
+
+`boiler`: is licensed under AGPL-3.0-only.
+`traincar`: is licensed under zLib with Aknowledgement.
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,18 @@
 const { Katana } = require("@chronomly/katana");
-const { Server } = require("ws");
+const { Server, WebSocket } = require("ws");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// allow CORS
+app.use(cors({ origin: true, credentials: true }));
 
 // prompt the user for the port
 const port = process.argv[2] || 8080;
 
 const channels = new Map();
 
-const datastore = new Katana("./database", { encrypt: true, saveToDisk: true });
+// const datastore = new Katana("./database", { encrypt: true, saveToDisk: true });
 
 const wss = new Server({ port: port });
 
@@ -45,14 +51,24 @@ wss.on("connection", (ws, req) => {
     }
 
     // log what channel the client is connecting to
-    console.log(`${username} connecting to: ${channelName}`);
+    console.log(`${username} connecting to: #${channelName}`);
 
     channel.set(username, ws);
+
+    // send the client a message to let them know they are connected
+    ws.send(`Welcome to #${channelName}, ${username}`);
+
+    // alert the other clients that a new client has joined
+    channel.forEach((client) => {
+        if (client !== ws) {
+            client.send(`${username} has joined the channel`);
+        }
+    });
 
     ws.on("message", (message) => {
         // check that the channel still exists
         if (!channels.has(channelName)) {
-            ws.send(`channel '${channelName}' does not exist, closing connection.'`)
+            ws.send(`Channel '${channelName}' does not exist, closing connection.'`)
             console.log(
                 `Channel '${channelName}' does not exist, closing connection`
             );
@@ -61,7 +77,7 @@ wss.on("connection", (ws, req) => {
         } else {
             // check that the channel still has the client
             if (!channel.has(username)) {
-                ws.send(`username '${username}' does not exist in channel '${channelName}, closing connection.'`)
+                ws.send(`username '${username}' does not exist in channel #${channelName}, closing connection.`)
                 console.log(`Client
                 '${username}' does not exist in channel '${channelName}', closing connection`);
                 ws.close();
@@ -105,13 +121,34 @@ wss.on("connection", (ws, req) => {
 
     ws.on("close", () => {
         console.log(`Client '${username}' disconnected`);
+
+        // alert the other clients that a client has left
+        channel.forEach((client) => {
+            if (client !== ws) {
+                client.send(`${username} has left the channel`);
+            }
+        });
+
         channel.delete(username);
     });
 });
 
-setInterval(() => {
+// create a route to get the number of channels
+app.get("/api/channels/size", (req, res) => {
+    res.send({ channels: channels.size });
+});
+
+// route to get all the channels
+app.get("/api/channels", (req, res) => {
+    res.send({ channels: [...channels.keys()] });
+
     // log the number of channels
     console.log(`Number of channels: ${channels.size}`);
+});
+
+setInterval(() => {
+    // log the number of channels
+    // console.log(`Number of channels: ${channels.size}`);
 
     for (const [channelName, channel] of channels) {
         // checking status of channel
@@ -129,4 +166,9 @@ setInterval(() => {
     }
 }, 5000);
 
+var listener = app.listen(port + 1, function () {
+    console.log('Server Functions API is listening on port ' + listener.address().port); //Listening on port 8888
+});
+
 console.log(`Server listening on port ${wss.address().port}`);
+
