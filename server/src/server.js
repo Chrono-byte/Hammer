38820bcab31a39a649d8101be58e52b4677a13e4
Chrono <me@disilla.org>
2023-01-19 01:28:44 -0500

    update server

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,3 @@
-const { Katana } = require("@chronomly/katana");
 const { Server, WebSocket } = require("ws");
 const express = require("express");
 const cors = require("cors");
@@ -12,162 +11,235 @@ const port = process.argv[2] || 8080;
 
 const channels = new Map();
 
-// const datastore = new Katana("./database", { encrypt: true, saveToDisk: true });
+// assemble message from "Server" to send to client
+function assembleServerMessage(message) {
+	return JSON.stringify({
+		timestamp: new Date().getTime(),
+		author: "Server",
+		message: message,
+	});
+}
+
 
 const wss = new Server({ port: port });
 
 wss.on("connection", (ws, req) => {
-    console.log("Client connected");
+	console.log("Client connected");
 
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const channelName = url.searchParams.get("channel");
-    let username = url.searchParams.get("username");
+	const url = new URL(req.url, `http://${req.headers.host}`);
+	const channelName = url.searchParams.get("channel");
+	let username = url.searchParams.get("username");
 
-    if (!channelName || !username) {
-        ws.close();
+	if (!channelName || !username) {
+		ws.close();
 
-        // log that we rejected the client
-        console.log(
-            `Client rejected because ${!channelName ? "channelName" : "username"
-            } was not provided`
-        );
+		// log that we rejected the client
+		console.log(
+			`Client rejected because ${!channelName ? "channelName" : "username"
+			} was not provided`
+		);
 
-        return;
-    }
+		return;
+	}
 
-    if (!channels.has(channelName)) {
-        channels.set(channelName, new Map());
-    }
+	if (!channels.has(channelName)) {
+		channels.set(channelName, new Map());
+	}
 
-    const channel = channels.get(channelName);
+	const channel = channels.get(channelName);
 
-    if (channel.has(username)) {
-        ws.send(`username '${username}' already taken, please choose another`);
+	if (channel.has(username)) {
+		ws.send(`username '${username}' already taken, please choose another`);
 
-        // log that we rejected the client
-        console.log(`Client '${username}' rejected`);
-        ws.close();
-        return;
-    }
+		// log that we rejected the client
+		console.log(`Client '${username}' rejected`);
+		ws.close();
+		return;
+	}
 
-    // log what channel the client is connecting to
-    console.log(`${username} connecting to: #${channelName}`);
+	// log what channel the client is connecting to
+	console.log(`${username} connecting to: #${channelName}`);
 
-    channel.set(username, ws);
+	channel.set(username, ws);
 
-    // send the client a message to let them know they are connected
-    ws.send(`Welcome to #${channelName}, ${username}`);
+	// send the client a message to let them know they are connected
+	ws.send(assembleServerMessage(`Welcome to #${channelName}, ${username}!`));
 
-    // alert the other clients that a new client has joined
-    channel.forEach((client) => {
-        if (client !== ws) {
-            client.send(`${username} has joined the channel`);
-        }
-    });
+	// send the client a message with the number of users in the channel
+	ws.send(assembleServerMessage(`There are ${channel.size - 1} other users in this channel.`));
 
-    ws.on("message", (message) => {
-        // check that the channel still exists
-        if (!channels.has(channelName)) {
-            ws.send(`Channel '${channelName}' does not exist, closing connection.'`)
-            console.log(
-                `Channel '${channelName}' does not exist, closing connection`
-            );
-            ws.close();
-            return;
-        } else {
-            // check that the channel still has the client
-            if (!channel.has(username)) {
-                ws.send(`username '${username}' does not exist in channel #${channelName}, closing connection.`)
-                console.log(`Client
+	// alert the other clients that a new client has joined
+	channel.forEach((client) => {
+		if (client !== ws) {
+			client.send(`${username} has joined the channel`);
+		}
+	});
+
+	ws.on("message", (message) => {
+		message = message.toString();
+
+		// check that the channel still exists
+		if (!channels.has(channelName)) {
+			ws.send(`Channel '${channelName}' does not exist, closing connection.'`)
+			console.log(
+				`Channel '${channelName}' does not exist, closing connection`
+			);
+			ws.close();
+			return;
+		} else {
+			// check that the channel still has the client
+			if (!channel.has(username)) {
+				ws.send(`username '${username}' does not exist in channel #${channelName}, closing connection.`)
+				console.log(`Client
                 '${username}' does not exist in channel '${channelName}', closing connection`);
-                ws.close();
-                return;
-            }
+				ws.close();
+				return;
+			}
 
-            // check if the message is a command
-            let checkCommand = `${message}}`;
-            if (checkCommand.split("")[0] == "/") {
-                const command = checkCommand.split(" ")[0].substring(1);
-                const args = checkCommand.split(" ").slice(1);
+			channel.forEach((client, name) => {
+				if (client.readyState === WebSocket.OPEN) {
+					client.send(JSON.stringify({
+						timestamp: new Date().getTime(),
+						author: username,
+						message: message,
+					}));
+				}
+			});
+		}
+	});
 
-                switch (command) {
-                    case "nick":
-                        if (args.length < 1) {
-                            ws.send("Please provide a username");
-                        }
+	ws.on("close", () => {
+		console.log(`Client '${username}' disconnected`);
 
-                        if (channel.has(args[0])) {
-                            ws.send(`username '${args[0]}' already taken, please choose another`);
-                            return;
-                        }
+		// alert the other clients that a client has left
+		channel.forEach((client) => {
+			if (client !== ws) {
+				client.send(`${username} has left the channel`);
+			}
+		});
 
-                        channel.delete(username);
-                        username = args[0];
-                        channel.set(username, ws);
-
-                        ws.send(`username changed to '${username}'`);
-                    default:
-                        break;
-                }
-            }
-
-            channel.forEach((client, name) => {
-                if (client.readyState === WebSocket.OPEN) {
-                    client.send(`${username}: ${message}`);
-                }
-            });
-        }
-    });
-
-    ws.on("close", () => {
-        console.log(`Client '${username}' disconnected`);
-
-        // alert the other clients that a client has left
-        channel.forEach((client) => {
-            if (client !== ws) {
-                client.send(`${username} has left the channel`);
-            }
-        });
-
-        channel.delete(username);
-    });
-});
-
-// create a route to get the number of channels
-app.get("/api/channels/size", (req, res) => {
-    res.send({ channels: channels.size });
+		channel.delete(username);
+	});
 });
 
 // route to get all the channels
-app.get("/api/channels", (req, res) => {
-    res.send({ channels: [...channels.keys()] });
+app.post("/api/channels", (req, res) => {
+	res.send({ channels: [...channels.keys()] });
+});
 
-    // log the number of channels
-    console.log(`Number of channels: ${channels.size}`);
+// route to create a channel
+app.post("/api/channels/create", (req, res) => {
+	const channelName = req.query.channel;
+
+	if (!channelName) {
+		res.status(400).send("Channel name not provided");
+		return;
+	}
+
+	if (channels.has(channelName)) {
+		res.status(400).send("Channel already exists");
+		return;
+	}
+
+	channels.set(channelName, new Map());
+
+	res.send("Channel created " + channelName);
+})
+
+// route to delete a channel
+app.post("/api/channels/delete", (req, res) => {
+	const channelName = req.query.channel;
+
+	if (!channelName) {
+		res.status(400).send("Channel name not provided");
+		return;
+	}
+
+	if (!channels.has(channelName)) {
+		res.status(400).send("Channel does not exist");
+		return;
+	}
+
+	channels.delete(channelName);
+
+	res.send("Channel deleted " + channelName);
+})
+
+// route to get the number of users in a channel
+app.post("/api/channels/users/size", (req, res) => {
+	const channelName = req.query.channel;
+
+	if (!channelName) {
+		res.status(400).send("Channel name not provided");
+		return;
+	}
+
+	if (!channels.has(channelName)) {
+		res.status(400).send("Channel does not exist");
+		return;
+	}
+
+	const channel = channels.get(channelName);
+
+	res.send({ users: channel.size });
+});
+
+// route to get all the users in a channel
+app.post("/api/channels/users", (req, res) => {
+	const channelName = req.query.channel;
+
+	if (!channelName) {
+		res.status(400).send("Channel name not provided");
+		return;
+	}
+
+	if (!channels.has(channelName)) {
+		res.status(400).send("Channel does not exist");
+		return;
+	}
+
+	const channel = channels.get(channelName);
+
+	res.send({
+		users: [...channel.keys()],
+	});
+});
+
+// route to get a channel
+app.post("/api/channels/get", (req, res) => {
+	const channelName = req.query.channel;
+
+	if (!channelName) {
+		res.status(400).send("Channel name not provided");
+		return;
+	}
+
+	if (!channels.has(channelName)) {
+		res.status(400).send("Channel does not exist");
+		return;
+	}
+
+	const channel = channels.get(channelName);
+
+	res.send({
+		channel: channel,
+	});
 });
 
 setInterval(() => {
-    // log the number of channels
-    // console.log(`Number of channels: ${channels.size}`);
+	for (const [channelName, channel] of channels) {
+		// if the channel is empty, delete it
+		if (channel.size < 1) {
+			console.log(`Channel '${channelName}' is empty, deleting.`);
 
-    for (const [channelName, channel] of channels) {
-        // checking status of channel
-        console.log(
-            "Channel: " + channelName + " has " + channel.size + " clients"
-        );
-
-        // if the channel is empty, delete it
-        if (channel.size < 1) {
-            console.log(`Channel '${channelName}' is empty, deleting.`);
-
-            channels.delete(channelName);
-            continue;
-        }
-    }
+			channels.delete(channelName);
+			continue;
+		}
+	}
 }, 5000);
 
 var listener = app.listen(port + 1, function () {
-    console.log('Server Functions API is listening on port ' + listener.address().port); //Listening on port 8888
+	console.log('Server Functions API is listening on port ' + listener.address().port); //Listening on port 8888
 });
 
 console.log(`Server listening on port ${wss.address().port}`);
